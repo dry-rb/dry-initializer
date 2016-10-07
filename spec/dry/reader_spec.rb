d96bc9ff9bf6596ca3dc91afa76a6a7e99c28feb
Jean-Michel Garnier <jean-michel@21croissants.com>
2016-10-07 16:18:48 +0200

    Add context to group tests for `reader: false`

--- spec/dry/reader_spec.rb
+++ spec/dry/reader_spec.rb
@@ -1,22 +1,24 @@
 describe "reader" do
-  subject do
-    class Test::Foo
-      extend Dry::Initializer::Mixin
+  context "with reader: false" do
+    subject do
+      class Test::Foo
+        extend Dry::Initializer::Mixin
 
-      param  :foo, reader: false
-      option :bar, reader: false
+        param  :foo, reader: false
+        option :bar, reader: false
+      end
+
+      Test::Foo.new 1, bar: 2
     end
 
-    Test::Foo.new 1, bar: 2
-  end
+    it "skips attr_reader" do
+      expect(subject).not_to respond_to :foo
+      expect(subject).not_to respond_to :bar
+    end
 
-  it "skips attr_reader" do
-    expect(subject).not_to respond_to :foo
-    expect(subject).not_to respond_to :bar
-  end
-
-  it "keeps assigning variables" do
-    expect(subject.instance_variable_get(:@foo)).to eql 1
-    expect(subject.instance_variable_get(:@bar)).to eql 2
+    it "keeps assigning variables" do
+      expect(subject.instance_variable_get(:@foo)).to eql 1
+      expect(subject.instance_variable_get(:@bar)).to eql 2
+    end
   end
 end
