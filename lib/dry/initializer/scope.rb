2a642537343075ab9f5f77c018fd23e00ae9fb2e
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-11-06 13:28:04 +0300

    Refactor scopes to allow method aliasing

--- lib/dry/initializer/scope.rb
+++ lib/dry/initializer/scope.rb
@@ -1,31 +1,20 @@
 module Dry::Initializer
   # Shared scope for several params and options
   class Scope
-    # Defines param with shared settings
-    #
-    # @param  (see Dry::Initializer::Mixin#param)
-    # @option (see Dry::Initializer::Mixin#param)
-    # @return (see Dry::Initializer::Mixin#param)
-    #
-    def param(name, **options)
-      @klass.param name, @options.merge(options)
-    end
-
-    # Defines option with shared settings
-    #
-    # @param  (see Dry::Initializer::Mixin#option)
-    # @option (see Dry::Initializer::Mixin#option)
-    # @return (see Dry::Initializer::Mixin#option)
-    #
-    def option(name, **options)
-      @klass.option name, @options.merge(options)
-    end
-
     private
 
     def initialize(klass, **options)
       @klass   = klass
       @options = options
+    end
+
+    def method_missing(name, *args, **options)
+      return super unless respond_to? name
+      @klass.send(name, *args, **@options.merge(options))
+    end
+
+    def respond_to_missing?(name, *)
+      @klass.respond_to? name
     end
   end
 end
