a92ba6b783eb97147d6e799e17f50d9427008d7a
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2017-01-02 20:32:00 +0300

    Update spec for initializer reloading

--- spec/custom_initializer_spec.rb
+++ spec/custom_initializer_spec.rb
@@ -13,13 +13,18 @@ describe "custom initializer" do
 
     class Test::Baz < Test::Foo
       param :qux
+
+      def initialize(*args)
+        super
+        @qux += 1
+      end
     end
   end
 
   it "reloads the initializer" do
-    baz = Test::Baz.new(2, 8)
+    baz = Test::Baz.new(5, 5)
 
-    expect(baz.bar).to eq 6 # 2 * 3
-    expect(baz.qux).to eq 8
+    expect(baz.bar).to eq 15 # 5 * 3
+    expect(baz.qux).to eq 6  # 5 + 1
   end
 end
