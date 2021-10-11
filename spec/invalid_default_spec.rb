1e6e9f2ee0e1881f908b6b5bc5ae49c439616c99
Svyatoslav Kryukov <s.g.kryukov@yandex.ru>
2021-10-11 10:50:13 +0300

    Fix arity check for defaults

--- spec/invalid_default_spec.rb
+++ spec/invalid_default_spec.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 describe "invalid default value assignment" do
+  shared_examples "it has a TypeError" do
+    it "raises TypeError" do
+      expect { subject }.to raise_error TypeError
+    end
+  end
+
   subject do
     class Test::Foo
       extend Dry::Initializer
@@ -7,7 +15,73 @@ describe "invalid default value assignment" do
     end
   end
 
-  it "raises TypeError" do
-    expect { subject }.to raise_error TypeError
+  it_behaves_like "it has a TypeError"
+
+  context "when default is a lambda one attribute with splat operator" do
+    subject do
+      class Test::Foo
+        extend Dry::Initializer
+
+        param :foo, default: ->(a) { a.to_i }
+      end
+    end
+
+    it_behaves_like "it has a TypeError"
+  end
+
+  context "when default is a proc with attributes" do
+    subject do
+      class Test::Foo
+        extend Dry::Initializer
+
+        param :foo, default: proc { |a| a.to_i }
+      end
+    end
+
+    it_behaves_like "it has a TypeError"
+  end
+
+  context "when default is a callable with attributes" do
+    subject do
+      class Test::Callbale
+        def self.call(a)
+          a.to_i
+        end
+      end
+
+      class Test::Foo
+        extend Dry::Initializer
+
+        param :foo, default: Test::Callbale
+      end
+    end
+
+    it_behaves_like "it has a TypeError"
+  end
+
+  context "when default is a proc with multiple attributes" do
+    subject do
+      class Test::Foo
+        extend Dry::Initializer
+
+        param :foo, default: proc { |a, *b| a.to_i }
+      end
+    end
+
+    it_behaves_like "it has a TypeError"
+  end
+
+  context "when default is a lambda one attribute with splat operator" do
+    subject do
+      class Test::Foo
+        extend Dry::Initializer
+
+        param :foo, default: ->(*a) { a.size }
+      end
+    end
+
+    it "does not raise TypeError" do
+      expect { subject }.not_to raise_error
+    end
   end
 end
