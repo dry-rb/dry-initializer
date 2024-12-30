ba4e000f80714daf708b7f934adab0b621393a69
Nikita Shilnikov <fg@flashgordon.ru>
2024-12-30 14:48:43 +0100

    Fix for ruby 3.1

--- lib/dry/initializer/mixin/root.rb
+++ lib/dry/initializer/mixin/root.rb
@@ -7,8 +7,16 @@ module Dry
       module Root
         private
 
-        def initialize(*, **)
-          __dry_initializer_initialize__(*, **)
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("3.2")
+          module_eval(<<~RUBY, __FILE__, __LINE__ + 1)
+            def initialize(*, **)
+              __dry_initializer_initialize__(*, **)
+            end
+          RUBY
+        else
+          def initialize(*args, **kwargs)
+            __dry_initializer_initialize__(*args, **kwargs)
+          end
         end
       end
     end
