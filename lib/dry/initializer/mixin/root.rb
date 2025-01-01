69eb4c550be4780d378f4e429231b2d8314e81ff
Nikita Shilnikov <fg@flashgordon.ru>
2025-01-01 19:34:44 +0100

    Always give names to parameters so that dry_auto-inject forwards dependencies
    An alternative would be a lot more complex, we'd need to construct a "correct" `initialize` signature which is really hard in presence of inheritance.

--- lib/dry/initializer/mixin/root.rb
+++ lib/dry/initializer/mixin/root.rb
@@ -7,16 +7,8 @@ module Dry
       module Root
         private
 
-        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("3.2")
-          module_eval(<<~RUBY, __FILE__, __LINE__ + 1)
-            def initialize(*, **)
-              __dry_initializer_initialize__(*, **)
-            end
-          RUBY
-        else
-          def initialize(*args, **kwargs)
-            __dry_initializer_initialize__(*args, **kwargs)
-          end
+        def initialize(*args, **kwargs)
+          __dry_initializer_initialize__(*args, **kwargs)
         end
       end
     end
